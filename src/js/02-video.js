fdcb1bf10d1e9004c975c6a7275e38fa1274b706
Alekstitula84 <alekstitula84@gmail.com>
2023-01-28 13:06:09 +0200

    uodate task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,28 +1,24 @@
-import Player from "@vimeo/player";
+import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'videoplayer-current-time';
 
 const iframe = document.querySelector('iframe');
-
 const player = new Player(iframe);
 
-const onPlay = function (data) {
-    localStorage.setItem('videoplayer-current-time', data.seconds);
-};
+function saveTime(data) {
+    const time = data.seconds;
+    localStorage.setItem(STORAGE_KEY, time);
+}
 
-player.on('timeupdate', throttle(onPlay, 1000));
+function updateTime() {
+    const currentTime = localStorage.getItem(STORAGE_KEY);
 
-const currentTime = Number(localStorage.getItem('videoplayer-current-time'));
-
-player.setCurrentTime(currentTime).then(function (seconds) {
-    // seconds = the actual time that the player seeked to
-}).catch(function (error) {
-    switch (error.name) {
-        case 'RangeError':
-            // the time was less than 0 or greater than the video’s duration
-            break;
-        default:
-            // some other error occurred
-            break;
+    if (currentTime) {
+        player.setCurrentTime(currentTime);
     }
-});
+}
+
+updateTime();
+
+player.on('timeupdate', throttle(saveTime, 1000));
